50c06a51534626805efa765b13488ef4378e170d
Sebastião Santos Lessa <93839108+Seblessa@users.noreply.github.com>
2022-11-01 01:38:08 +0000

    Add files via upload

---
+++ Volume3Mooshak/Pim Pam Pum/src/ED006.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class ED006 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        int n;
+        input.nextLine();
+        int p=100;
+        String frase;
+        String[] palavras;
+
+        CircularLinkedList<String> list = new CircularLinkedList<>();
+
+        for (int i=0;i<N;i++){
+            frase=input.nextLine();
+            palavras=frase.split(" ");
+            n=input.nextInt();
+            boolean bool=false;
+            for (int j=0;j<n;j++){
+                list.addLast(input.next());
+            }
+            while(list.size()!=1){
+                for (int j=0;j<palavras.length-1;j++){
+                    list.rotate();
+                }
+                list.removeFirst();
+            }
+            if (list.getFirst().equals("Carlos")){
+                System.out.println("O Carlos nao se livrou");
+            }else{
+                System.out.println("O Carlos livrou-se (coitado do "+list.getFirst()+"!)");
+            }
+            input.nextLine();
+            while (!(list.isEmpty())){
+                list.removeFirst();
+            }
+        }
+
+
+
+
+    }
+}
